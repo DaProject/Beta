79560736215edbbe1ed99e0339cf828ac2b44f25
Puxi22 <alexpuighibetdesigns@gmail.com>
2016-04-24 19:02:28 +0200

    gdfsdgsd

---
+++ Beta/Assets/Resources/Scripts/Character/TrailActivation.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrailActivation : MonoBehaviour {
+
+    public PlayerManagerBackup playerManagerBackup;
+
+	// Use this for initialization
+	void Start ()
+    {
+        playerManagerBackup = GetComponentInParent<PlayerManagerBackup>();
+      
+        gameObject.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (playerManagerBackup.trailActivation == true)
+        {
+            Debug.Log("trail activation!!!!");
+
+            gameObject.SetActive(true);
+        }
+
+       // else if (playerAttack.trailActivation == false)
+        //{
+
+        //    gameObject.SetActive(false);
+       // }
+
+        else gameObject.SetActive(false);
+    }
+}
